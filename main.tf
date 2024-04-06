337ae103de56b4735a54cefe9621d1fb52a6123d
ryanef <ryanef39@gmail.com>
2024-04-06 19:09:07 -0400

    Secgr (#16)
    * security group
    
    * security groups

--- main.tf
+++ main.tf
@@ -143,18 +143,12 @@ resource "aws_security_group" "default" {
   }
 
   ingress {
-    protocol = "-1"
-    from_port = 0
-    to_port = 0
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
     cidr_blocks = ["${var.vpc_cidr}"]
   }
 
-  egress {
-    protocol = "-1"
-    from_port = 0
-    to_port = 0
-    cidr_blocks = ["${var.vpc_cidr}"]
-  }
   egress {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
