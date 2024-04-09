29b42b0490423fcca82f0a24665a8381fe60d705
ryanef <ryanef39@gmail.com>
2024-04-09 10:37:14 -0400

    flow logs (#19)

--- data.tf
+++ data.tf
@@ -2,3 +2,30 @@ data "aws_availability_zones" "available" {
   state         = "available"
   exclude_names = var.az_exclude_names
 }
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+data "aws_iam_policy_document" "cwlogs" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+    ]
+
+    resources = ["*"]
+  }
+}
